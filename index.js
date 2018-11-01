68209f89b21b7ced771d3fa832a21d554f368b2e
ssong-eu <sangyoon.song@gmail.com>
2018-11-01 17:36:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'b3a2960e60e08436c14db3d52c095bd487622016'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
