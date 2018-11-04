5630077cf0beda79b1af4685f50a3838791def98
anavarreteuk <a.navarrete.uk@gmail.com>
2018-11-04 22:18:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+
+const token = '9288fcb1ec703af0a288dd93f6def357193514ab'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
