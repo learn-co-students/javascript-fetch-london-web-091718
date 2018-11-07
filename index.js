508ea7f838669de08747d1f34b862cb604c2eaf1
Irum <irum@Irums-MacBook-Air.local>
2018-11-07 14:26:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+
+const token = "7f20d66061e4e8cd0361a7a789101a29b03d2fdb"
+fetch('https://api.github.com/repos',
+{
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
